898ebe360c3fc3e1124058ca2de542dcf07d73f0
skipsuva <skip.suva@gmail.com>
2016-09-14 07:13:54 -0400

    generate client-side template, add ENV

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module SpotifyRememberWeekly
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+
+    RSpotify::authenticate(ENV[SPOTIFY_CLIENT_ID], ENV[SPOTIFY_CLIENT_SECRET])
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
