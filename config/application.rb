46aa956f83145d4a83488d6d0535e6ecfa4c6f47
skipsuva <skip.suva@gmail.com>
2016-09-14 07:36:07 -0400

    add route for callback, add omniauth

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module SpotifyRememberWeekly
     # config.i18n.default_locale = :de
 
 
-    RSpotify::authenticate(ENV[SPOTIFY_CLIENT_ID], ENV[SPOTIFY_CLIENT_SECRET])
+    # RSpotify::authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
+
+  get '/auth/spotify/callback', to: 'users#spotify'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
